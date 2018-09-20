3e956b129b58bb657b4b0e051b731f7d5fd4fb3a
Andrée Asmat <andree1320z@gmail.com>
2018-09-20 15:05:24 -0500

    Initial

---
+++ app/src/main/java/pe/edu/upao/andree/segundaclase/MainActivity.java
@@ -0,0 +1,13 @@
+package pe.edu.upao.andree.segundaclase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
